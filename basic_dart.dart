ae0627b90468e6f9807cab99dd65f73f554ee564
Binisha Chapagain <bich_bca2079@lict.edu.np>
2025-03-28 11:06:09 +0545

    flutter begin

---
+++ basic_dart.dart
@@ -0,0 +1,15 @@
+void main() {
+  //two number add
+  // int a = 10;
+  // int b = 10;
+  // int c = a + b;
+  // print(c);
+
+  //check if the number is greater than 50 or not
+  int a = 70;
+  if (a > 50) {
+    print("Number is greater than 50");
+  } else {
+    print("number is less tha 50  ");
+  }
+}
